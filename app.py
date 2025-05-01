b359ac10dbdfdb35ffb009ede1080564ae082190
Seb45 <59327242+Seb45@users.noreply.github.com>
2025-05-01 20:51:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def get_llm():
     try:
         # Ajusta temperature y top_p según necesites
         return ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", google_api_key=GOOGLE_API_KEY,
-                                      temperature=0.2, top_p=0.5)
+                                      temperature=0.5, top_p=0.9)
     except Exception as e:
         st.error(f"Error al cargar el LLM Gemini: {e}")
         return None
